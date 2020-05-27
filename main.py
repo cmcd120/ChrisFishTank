aeb83047024dcd27d0b7a333c1fae4d11f2fba1a
null null <mcdonaldch@bearsmail.org>
2020-05-27 17:38:14 +0000

    Made the fish bounce around and flip around

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random 
 
 pygame.init() 
 
@@ -6,25 +6,52 @@ screen_info=pygame.display.Info()
 
 screen_size=(width,height)=(screen_info.current_w, screen_info.current_h) 
 
+#Clock to set frame rate 
+clock=pygame.time.Clock()
+
 fish_image=pygame.image.load("fish.png")
 fish_image=pygame.transform.smoothscale(fish_image,(100,100))
 fish_rect=fish_image.get_rect()
 fish_rect.center=(width//2,height//2)
 
-speed=pygame.math.Vector2(0,10)
-roation=random.randint(0,360)
+#variables to move fish
+speed=pygame.math.Vector2(30,5)
+rotation=random.randint(0,360)
+speed.rotate_ip(rotation)
+color=(153,204,7) 
+screen=pygame.display.set_mode(screen_size)
 
+fish_image=pygame.transform.rotate(fish_image,180-rotation)
+
+#define what happens when we move the fish
 def move_fish():
   global fish_image 
   screen_info=pygame.display.Info() 
-  fish_rect.move_ip(speed) 
-  if fish_rect.left <= 0 or fish_rect.right > screen_info.current_w:
+  fish_rect.move_ip(speed)  
 
-screen=pygame.display.set_mode(screen_size)
-color=(153,204,7) 
+
+  #if fish hits top or bottom
+  if fish_rect.top < 0 or fish_rect.bottom > screen_info.current_h:
+    speed[1] *= -1
+    fish_rect.move_ip(0, speed[1])
+    fish_image=pygame.transform.flip(fish_image, True, False)
+
+ #if fish hits left or right
+  if fish_rect.left < 0 or fish_rect.right > screen_info. current_w:
+    speed[0] *= -1
+    fish_rect.move_ip(speed[0], 0) 
+    fish_image=pygame.transform.flip(fish_image, False, True)
+
+
+  #if fish_rect.left <= 0 or fish_rect.right > screen_info.current_w:
+
+
+
 
 def main():
-  while True:
+  while True: 
+    clock.tick(60)
+    move_fish()
     screen.fill(color)
     screen.blit(fish_image,fish_rect)
     pygame.display.flip()
